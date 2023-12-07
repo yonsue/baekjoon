123c248d0f3a9ac3401e66ac7e9c993abf1a1b06
yonsue <bananarang523@gmail.com>
2023-12-07 10:49:32 +0900

    Initial commit

--- src/main/java/조건문/baekjoon1330.java
+++ src/main/java/조건문/baekjoon1330.java
@@ -1,4 +1,33 @@
 package 조건문;
-
+import java.util.Scanner;
 public class baekjoon1330 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("A의 값을 입력하시오.");
+        int A = scanner.nextInt();
+        System.out.println("B의 값을 입력하시오.");
+        int B = scanner.nextInt();
+/*
+        // if -> else -> if -> else
+        if(A>B){
+            System.out.println("A>B");
+        }else{
+            if(A<B){
+                System.out.println("A<B");
+            }else{
+                System.out.println("A==B");
+            }
+        }
+ */
+        // if -> else if -> else : 이쪽이 코드가 간결하고 가독성이 좋다
+        if(A>B){
+            System.out.println("A>B");
+        } else if (A<B) {
+            System.out.println("A<B");
+        }else {
+            System.out.println("A==B");
+        }
+    }
+
 }
